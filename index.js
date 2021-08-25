82b7b2edb47036cbafda67c73befabfebd6c0a99
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2021-08-25 22:34:15 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const upload = require('express-fileupload');
+const path = require('path');
+const fs = require('fs');
+
+const dir = './uploads';
+if (!fs.existsSync(dir)) {
+	fs.mkdirSync(dir, {
+		recursive: true
+	});
+}
+
+const app = express();
+
+app.use(upload());
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/dfw_ost-pic.html');
+});
+/*
+app.get('/image',(req,res) => {
+    res.sendFile(__dirname + '/dfw_ost-pic2.html');
+}); */
+
+app.post('/', (req,res) =>{
+    if(req.files){
+        console.log(req.files);
+        let file = req.files.file;
+        let filename = file.name;
+        let type = file.mimetype;
+        console.log(filename);
+       // file.name = 'image.png';
+        console.log(type);
+
+        filename = 'image.png';
+        console.log(filename);
+
+        // target_file.mv(path, callback)
+        /*
+        target_file.mv(path.join(__dirname, 'uploads', filename), (err) => {
+            if (err) throw err;
+            res.send('File Uploaded');
+        })*/
+
+        file.mv(path.join(__dirname, 'uploads', filename), (err) =>{
+            if (err){
+                res.send(err);
+            } else{
+                res.send('File Uploaded');
+                app.use('/uploads', express.static(path.join(__dirname,'static')));
+
+                app.get('/images', (req,res)=>{
+                res.sendFile(path.join(__dirname,'uploads','image.png'));
+});
+            }
+        });
+    }
+})
+
+// file.mimetype
+
+
+
+app.listen(5000);
