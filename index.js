25a4022cc01385a10d64ee21a2e1037086b09aa6
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2021-08-25 23:37:16 +0400

    unique url for each image commit
    made it so that each image will have a unique url
    
    u just gotta use the name that is logged in console

--- index.js
+++ index.js
@@ -1,51 +1,66 @@
-const express = require('express');
-const upload = require('express-fileupload');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const upload = require("express-fileupload");
+const path = require("path");
+const fs = require("fs");
 
-const dir = './uploads';
+// function to make directory if it doesnt exist
+const dir = "./uploads";
 if (!fs.existsSync(dir)) {
-	fs.mkdirSync(dir, {
-		recursive: true
-	});
+  fs.mkdirSync(dir, {
+    recursive: true,
+  });
 }
 
 const app = express();
 
 app.use(upload());
 
-app.get('/',(req,res) => {
-    res.sendFile(__dirname + '/dfw_ost-pic.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/dfw_ost-pic.html");
 });
 
-app.post('/', (req,res) =>{
-    if(req.files){
-        console.log(req.files);
-        let file = req.files.file;
-        let filename = file.name;
-        let type = file.mimetype;
-        console.log(filename);
-       // file.name = 'image.png';
-        console.log(type);
-        let time = new Date().getTime();
-        console.log(time);
-        filename = time + 'image.png';
-        console.log(filename);
+app.post("/", (req, res) => {
+  if (req.files) {
+    console.log(req.files);
+    let file = req.files.file;
+    let filename = file.name;
+    let type = file.mimetype;
+    console.log(filename);
 
-        file.mv(path.join(__dirname, 'uploads', filename), (err) =>{
-            if (err){
-                res.send(err);
-            } else{
-                res.send('File Uploaded');
-                app.use('/uploads', express.static(path.join(__dirname,'static')));
+    console.log(type);
+    let time = new Date().getTime();
+    console.log(time);
+    filename = time + "image.png";
+    console.log(filename);
 
-                app.get('/images', (req,res)=>{
-                res.sendFile(path.join(__dirname, 'uploads' , filename));
-});
-            }
+    file.mv(path.join(__dirname, "uploads", filename), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("File Uploaded");
+        app.use("/uploads", express.static(path.join(__dirname, "static"))); // static folder
+
+        //joining path of directory
+        const directoryPath = path.join(__dirname, "uploads");
+        //passsing directoryPath and callback function
+        fs.readdir(directoryPath, function (err, files) {
+          //handling error
+          if (err) {
+            return console.log("Unable to scan directory: " + err);
+          }
+          //listing all files using forEach
+          files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            console.log(file);
+            app.get(`/image-${file}`, (req, res) => {
+                res.sendFile(path.join(__dirname, "uploads", file));
+              });
+          });
         });
-    }
-})
+      }
+    });
+  }
+});
 
 // file.mimetype
 
