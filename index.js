cf3ac8c72b7abd94814dae555abdcc53be5bbca4
vpkaushik11 <83803726+vpkaushik11@users.noreply.github.com>
2021-08-25 19:51:34 +0400

    Second Commit
    Added multiple URL to see image
    using a new html file
    
    added some CSS

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const upload = require('express-fileupload');
+const path = require('path');
+
+const app = express();
+
+app.use(upload());
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/dfw_ost-pic.html');
+});
+
+app.get('/image',(req,res) => {
+    res.sendFile(__dirname + '/dfw_ost-pic2.html');
+});
+
+app.post('/', (req,res) =>{
+    if(req.files){
+        console.log(req.files);
+        let file = req.files.file;
+        let filename = file.name;
+        let type = file.mimetype;
+        console.log(filename);
+       // file.name = 'image.png';
+        console.log(filename);
+        console.log(type);
+
+        filename = 'image.png';
+
+        // target_file.mv(path, callback)
+        /*
+        target_file.mv(path.join(__dirname, 'uploads', filename), (err) => {
+            if (err) throw err;
+            res.send('File Uploaded');
+        })*/
+
+        file.mv(path.join(__dirname, 'uploads', filename), (err) =>{
+            if (err){
+                res.send(err);
+            } else{
+                res.send('File Uploaded');
+                
+            }
+        });
+    }
+})
+
+// file.mimetype
+
+app.listen(5000);
